1025c025726cdd97d95bfd2da001e194adecc6e1
vunv14 <vunvph49243@gmail.com>
2025-10-09 23:40:52 +0700

    feat: new logic userdetailserice and authenticationProvider

--- user-service/user-service/src/main/java/user_service/user_service/entities/Role.java
+++ user-service/user-service/src/main/java/user_service/user_service/entities/Role.java
@@ -1,11 +1,23 @@
 package user_service.user_service.entities;
 
-/**
- * Created by vunv on 10/2/2025
- */
-public enum Role {
+import com.example.commonservice.entities.base.PrimaryEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-    USER,
-    ADMIN
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role extends PrimaryEntity implements Serializable {
+
+    private String name;
 
 }
